053ea46c9240c93a17d1c8e66982e4dbeba9ad7b
flouressaint <kadomtcev.va@students.dvfu.ru>
2024-04-27 13:47:15 +1000

    fix entities

--- src/DataFixtures/AuditoriumFixtures.php
+++ src/DataFixtures/AuditoriumFixtures.php
@@ -8,12 +8,21 @@ use Doctrine\Persistence\ObjectManager;
 
 class AuditoriumFixtures extends Fixture
 {
+    final public const AUDITORIUMS = [
+        'kab 1',
+        'kab 2',
+        'kab 3',
+        'kab 4',
+    ];
+
     public function load(ObjectManager $manager): void
     {
-        $manager->persist((new Auditorium())->setName('kab 1'));
-        $manager->persist((new Auditorium())->setName('kab 2'));
-        $manager->persist((new Auditorium())->setName('kab 3'));
-        $manager->persist((new Auditorium())->setName('kab 4'));
+        foreach (self::AUDITORIUMS as $key => $name) {
+            $auditorium = (new Auditorium())
+                ->setName($name);
+            $manager->persist($auditorium);
+            $this->addReference($key, $auditorium);
+        }
 
         $manager->flush();
     }
---
+++ src/DataFixtures/LessonFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class LessonFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            AuditoriumFixtures::class,
+            DisciplineFixtures::class,
+        ];
+    }
+}
